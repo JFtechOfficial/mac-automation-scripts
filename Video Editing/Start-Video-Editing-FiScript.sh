a1cb89f62bf8b2b8121f8f7d325c2fba93b3dbb7
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2020-02-22 13:36:05 +0100

    Add files via upload

---
+++ Video Editing/Start-Video-Editing-FiScript.sh
@@ -0,0 +1,6 @@
+open /Library/PreferencePanes/USB\ Overdrive.prefPane/Contents/Resources/USBOverdriveHelper.app
+open /Applications/TouchOSC\ Bridge.app
+open /Applications/CommandPost.app
+open $PATH
+delay 2
+open "btt://trigger_named/?trigger_name=VIDEO_EDITING"
